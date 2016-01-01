5c5d0decbdc7aa46448f3753a0926ecf468556c2
Gerard Vico <gerard.vico@gmail.com>
2016-01-01 18:29:10 +0100

    Orika mapper example

---
+++ src/main/java/com/hdbandit/orika_mapper/PersonCompleteInfoDTO.java
@@ -0,0 +1,81 @@
+package com.hdbandit.orika_mapper;
+
+public class PersonCompleteInfoDTO {
+
+    private String name;
+    private String surname;
+    private int age;
+    private String address;
+    private String jobLocation;
+    private int salary;
+    private String jobCategory;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getJobLocation() {
+        return jobLocation;
+    }
+
+    public void setJobLocation(String jobLocation) {
+        this.jobLocation = jobLocation;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getJobCategory() {
+        return jobCategory;
+    }
+
+    public void setJobCategory(String jobCategory) {
+        this.jobCategory = jobCategory;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonCompleteInfoDTO{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", jobLocation='" + jobLocation + '\'' +
+                ", salary=" + salary +
+                ", jobCategory='" + jobCategory + '\'' +
+                '}';
+    }
+}
